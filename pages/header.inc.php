f14ac3c952634c139699da8ba5d1a64cbd6ecd1e
Michal Paluchowski <Michal.Paluchowski@stepstone.com>
2016-09-18 07:43:22 +0200

    Upgrade jQuery

--- pages/header.inc.php
+++ pages/header.inc.php
@@ -10,8 +10,8 @@
 	<link rel="stylesheet" href="/screen.css">
 
 <?php if (is_admin()): ?>
-	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
-	<script src="/lib.js" type="text/javascript"></script>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+	<script type="text/javascript" src="/lib.js"></script>
 	<script type="text/javascript">
 		$(document).ready(function(){
 			toastmasters.admin.init();
